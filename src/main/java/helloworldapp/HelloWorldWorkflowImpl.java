fd1824c4b3e9d8d15c7f97399a81ba37aab84eb8
george samy <56982844+ge0rge000@users.noreply.github.com>
2021-09-09 13:04:55 +0200

    edit new two

--- src/main/java/helloworldapp/HelloWorldWorkflowImpl.java
+++ src/main/java/helloworldapp/HelloWorldWorkflowImpl.java
@@ -12,10 +12,9 @@ public class HelloWorldWorkflowImpl implements HelloWorldWorkflow {
             .build();
 
     // ActivityStubs enable calls to Activities as if they are local methods, but actually perform an RPC.
-    private final Format format = Workflow.newActivityStub(Format.class, options);
-    private final Printactivity printactivity=Workflow.newActivityStub(Printactivity.class,options);
+  
     private final Api currency=Workflow.newActivityStub(Api.class,options);
-    private final GreetingActivities activities =Workflow.newActivityStub(GreetingActivities.class,options);
+
 
     @Override
     public String getGreeting(String name) {
